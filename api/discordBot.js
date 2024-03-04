ced4cb19a49754bfd2f62dbebf4cc472e3750c03
Hadley99 <hadleyfernandes99@gmail.com>
2024-03-04 19:32:16 +0530

    minor change

--- api/discordBot.js
+++ api/discordBot.js
@@ -33,6 +33,7 @@ export default async (req, res) => {
       });
     })
     .then(() => {
-      return res.send('Bot finished running!');
+      return new Response('Bot finished running!');
+      // return res.status(200)('Bot finished running!');
     });
 };
