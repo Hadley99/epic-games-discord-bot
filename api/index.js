0d3dabe7f3572f58b7a31b7b3cbf6e774fd97c5e
Hadley99 <hadleyfernandes99@gmail.com>
2024-03-05 08:14:04 +0530

    // app.listen

--- api/index.js
+++ api/index.js
@@ -17,6 +17,6 @@ app.get(
   // (req, res) => res.send('Discord Bot is running!')
 );
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+// app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 export default app;
