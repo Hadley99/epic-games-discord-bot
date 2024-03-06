5958620619f17e494403e0b30c06bc646e6f519d
Hadley99 <hadleyfernandes99@gmail.com>
2024-03-06 09:43:07 +0530

    added webhook

--- api/discordBot.js
+++ api/discordBot.js
@@ -1,39 +1,32 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { WebhookClient } from 'discord.js';
 import { getFreeGames, createEmbedObjects } from '../getGames.js';
 
 import { config } from 'dotenv';
 config();
 
 export default async (req, res) => {
-  const games = await getFreeGames();
-  const freeGames = games.filter(({ isFreeGame }) => isFreeGame);
+  try {
+    const games = await getFreeGames();
+    const freeGames = games.filter(({ isFreeGame }) => isFreeGame);
 
-  const embedGames = freeGames.map(createEmbedObjects);
-
-  const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-  });
-
-  return client
-    .login(process.env.BOT_TOKEN)
-    .then(() => {
-      client.once('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-
-        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
-
-        return channel
-          .send({
-            embeds: embedGames,
-          })
-          .then(async () => {
-            console.log('ran');
-            // return await client.destroy();
-          });
-      });
-    })
-    .then(() => {
-      // return new Response('Bot finished running!');
-      return res.send('Bot finished running!');
+    const embedGames = freeGames.map(createEmbedObjects);
+    const webhook = new WebhookClient({
+      id: '1214411053161652286',
+      token:
+        '4ZBe4-DuwTv2hUR05p02v0GYyaLebEIA5X4IwisvVWtwGu9tIwZa3ladxy0_BMliiOyZ',
     });
+
+    webhook
+      .send({
+        embeds: embedGames,
+      })
+      .then(() => {
+        return res.send('Bot finished running!');
+      })
+      .catch((error) => {
+        throw error;
+      });
+  } catch (error) {
+    throw error;
+  }
 };
